1fadf5149aecfae044c93171a822521f033005b0
Sebastian <sebastiandiaz1222@gmail.com>
2026-08-13 14:38:16 -0700

    Feat: betterauth with google login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,15 @@
+"use client"
+
+import { createAuthClient } from "better-auth/client";
+const authClient = createAuthClient();
+
 export default function Page() {
 	return (
-		<h1>Hello login!</h1>
+		<>
+			<h1>Hello login!</h1>
+			<button onClick={() => authClient.signIn.social({provider: "google",})}>
+				Login With Google 
+			</button>
+		</>
 	);
 }
