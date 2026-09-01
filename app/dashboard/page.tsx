2a8389d9512b11fb0b5fd3bcba7672646e49d5c0
Sebastian <sebastiandiaz1222@gmail.com>
2026-08-31 19:31:24 -0700

    feat: get user data with useSession

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,26 @@
 "use client"
 
-import { signOut } from "@/lib/auth-client"
-import Button from "@/components/Button"
+import { signOut, useSession } from "@/lib/auth-client"
 import { useRouter } from "next/navigation"
+import Button from "@/components/Button"
+import Image from "next/image"
 
 export default function Page() {
-    const router = useRouter();
+    const { data: session, isPending } = useSession()
+    const router = useRouter()
+
+    if (isPending) return <div>Loading...</div>
+
     return (
         <div>
-            <h1>hello dashboard</h1>
+            <h1>Welcome {session?.user?.name}</h1>
+            <Image 
+                src={session?.user?.image ?? "/default-pfp.jpg"} 
+                alt="profile picture"
+                width={40}
+                height={40}
+                className="rounded-full"
+            />
             <Button onClick={() => signOut({
                 fetchOptions: { onSuccess: () => router.push("/login") }
             })}>
